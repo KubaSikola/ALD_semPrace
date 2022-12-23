31567bf018e8c5fd84acc45a7cb467343f745b8b
Jakub Sikola <jakub.sikola@tul.cz>
2022-12-23 18:28:58 +0100

    small changes

--- src/utils/TileStorage.java
+++ src/utils/TileStorage.java
@@ -7,13 +7,9 @@ public class TileStorage {
     public static ArrayList<Tile> allTiles = new ArrayList();
     
     public static ArrayList<Tile> upPipe = new ArrayList();
-    public static ArrayList<Tile> rightPipe = new ArrayList();
-    public static ArrayList<Tile> downPipe = new ArrayList();
     public static ArrayList<Tile> leftPipe = new ArrayList();
     
     public static ArrayList<Tile> upNone = new ArrayList();
-    public static ArrayList<Tile> rightNone = new ArrayList();
-    public static ArrayList<Tile> downNone = new ArrayList();
     public static ArrayList<Tile> leftNone = new ArrayList();
 
     static int xIndex = 0;
@@ -36,19 +32,7 @@ public class TileStorage {
             }else{
                 upNone.add(t);
             }
-            
-            if(t.getTileRight() == 1){
-                rightPipe.add(t);
-            }else{
-                rightNone.add(t);
-            }
-             
-            if(t.getTileDown() == 1){
-                downPipe.add(t);
-            }else{
-                downNone.add(t);
-            }
-              
+
             if(t.getTileLeft() == 1){
                 leftPipe.add(t);
             }else{
